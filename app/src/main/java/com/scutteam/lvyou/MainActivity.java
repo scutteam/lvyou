78d70b66ef94f63341c331b150bca43475462819
liujiescut <969343185@qq.com>
2015-07-17 11:11:27 +0800

    add Sync HTTP & ActiveAndroid to project

---
+++ app/src/main/java/com/scutteam/lvyou/MainActivity.java
@@ -0,0 +1,23 @@
+package com.scutteam.lvyou;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.activeandroid.ActiveAndroid;
+import com.loopj.android.http.SyncHttpClient;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //下面两行只是为了测试
+        SyncHttpClient client = new SyncHttpClient();
+        ActiveAndroid.initialize(this);
+    }
+}
