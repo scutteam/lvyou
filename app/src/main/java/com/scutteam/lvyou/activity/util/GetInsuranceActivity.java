9dcf1eaef6dd68f8d32394ea1b74574c63337f84
liujie <969343185@qq.com>
2015-08-15 08:52:33 +0800

    liujie

---
+++ app/src/main/java/com/scutteam/lvyou/activity/util/GetInsuranceActivity.java
@@ -0,0 +1,19 @@
+package com.scutteam.lvyou.activity.util;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.scutteam.lvyou.R;
+
+public class GetInsuranceActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_insurance);
+    }
+
+}
